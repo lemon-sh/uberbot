a77dc2ad7e6237150a46307799b0badfa0bbd208
lemon-sh <lemon@lemonsh.moe>
2021-12-27 19:08:46 +0100

    Implement basic command framework & add waifu command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
 
-use irc::client::{Client, prelude::Config, ClientStream};
 use futures::stream::StreamExt;
+use irc::{
+    client::{prelude::Config, Client, ClientStream},
+    proto::{Command, Message},
+};
 use tokio::select;
-use tracing::{debug, info, error};
+use tracing::{debug, error, info, warn};
 use tracing_subscriber::EnvFilter;
+
+mod waifu;
 
 #[cfg(unix)]
 async fn terminate_signal() {
@@ -26,30 +31,93 @@ async fn terminate_signal() {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt().with_env_filter(EnvFilter::from_env("UBERBOT_LOG")).init();
-    let config = Config::load(env::var("UBERBOT_CONFIG").unwrap_or_else(|_| "uberbot.toml".to_owned()))?;
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_env("UBERBOT_LOG"))
+        .init();
+    let mut config =
+        Config::load(env::var("UBERBOT_CONFIG").unwrap_or_else(|_| "uberbot.toml".to_owned()))?;
+    let prefix = config.options.remove("prefix").unwrap_or("!".into());
+    let prefix = prefix.as_str();
 
     let mut client = Client::from_config(config).await?;
     client.identify()?;
     let stream = client.stream()?;
 
-    select! {
-        r = message_loop(stream) => {
-            if let Err(e) = r {
-                error!("Error in message loop: {}", e);
-            }
-        }
-        _ = terminate_signal() => {}
+    if let Err(e) = message_loop(stream, prefix, client).await {
+        error!("Error in message loop: {}", e);
     }
 
-    info!("Shutting down...");
+    info!("Shutting down");
 
     Ok(())
 }
 
-async fn message_loop(mut stream: ClientStream) -> anyhow::Result<()> {
-    while let Some(message) = stream.next().await.transpose()? {
-        debug!("Received message: {}", message);
+async fn message_loop(
+    mut stream: ClientStream,
+    prefix: &str,
+    client: Client,
+) -> anyhow::Result<()> {
+    loop {
+        select! {
+            r = stream.next() => {
+                if let Some(message) = r.transpose()? {
+                    debug!("{}", message.to_string().trim_end());
+
+                    if let Err(e) = handle_message(message, prefix, &client).await {
+                        warn!("Error in message handler: {}", e);
+                    }
+                } else {
+                    break
+                }
+            },
+            _ = terminate_signal() => {
+                info!("Sending QUIT message");
+                client.send_quit("überbot shutting down")?;
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn handle_message(msg: Message, prefix: &str, client: &Client) -> anyhow::Result<()> {
+    if let Command::PRIVMSG(target, content) = &msg.command {
+        let target = msg.response_target().unwrap_or(target);
+        if let Err(e) = handle_privmsg(target, content, prefix, client).await {
+            client.send_privmsg(target, format!("Error: {}", e))?;
+        }
+    }
+    Ok(())
+}
+
+async fn handle_privmsg(
+    target: &str,
+    content: &String,
+    prefix: &str,
+    client: &Client,
+) -> anyhow::Result<()> {
+    if !content.starts_with(prefix) {
+        return Ok(());
+    }
+    let content = content.trim();
+    let space_index = content.find(' ');
+    let (command, remainder) = if let Some(o) = space_index {
+        (&content[prefix.len()..o], Some(&content[o + 1..]))
+    } else {
+        (&content[prefix.len()..], None)
+    };
+    debug!("Command received ({}; {:?})", command, remainder);
+
+    match command {
+        "waifu" => {
+            let category = remainder.unwrap_or("waifu");
+            let url = waifu::get_waifu_pic(category).await?;
+            let response = url.as_ref().map(|v| v.as_str())
+                .unwrap_or("Invalid category. Valid categories: https://waifu.pics/docs");
+            client.send_privmsg(target, response)?;
+        }
+        _ => {
+            client.send_privmsg(target, "Unknown command")?;
+        }
     }
     Ok(())
 }
---
+++ src/waifu.rs
@@ -0,0 +1,11 @@
+use serde_json::Value;
+use tracing::debug;
+
+pub async fn get_waifu_pic(category: &str) -> anyhow::Result<Option<String>> {
+    let api_resp = reqwest::get(format!("https://api.waifu.pics/sfw/{}", category)).await?.text().await?;
+    let api_resp = api_resp.trim();
+    debug!("API response: {}", api_resp);
+    let value: Value = serde_json::from_str(&api_resp)?;
+    let url = value["url"].as_str().map(|v| v.to_string());
+    Ok(url)
+}
