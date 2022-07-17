508c6dc0fba46ea6278ea2826068dbdbe1a07c7e
lemonsh <lemon@lemonsh.moe>
2022-07-17 21:00:27 +0200

    Update help command

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -3,10 +3,10 @@ use async_trait::async_trait;
 
 const HELP: &str = concat!(
     "=- \x1d\x02Überbot\x0f ", env!("CARGO_PKG_VERSION"), " -=\r\n",
-    " * waifu <category>\r\n",
-    " * owo/mock/leet [user]\r\n",
-    " * ev <math expression>\r\n",
-    " - This bot also provides titles of URLs and details for Spotify URIs/links. It can also resolve sed expressions."
+    " * waifu <category>      * grab [count] <user>\r\n",
+    " * owo/mock/leet [user]  * quot <user>\r\n",
+    " * ev <math expression>  * qsearch <query>\r\n",
+    " - This bot can also resolve HTML titles, Spotify links and a subset of sed expressions."
 );
 
 pub struct Help;
