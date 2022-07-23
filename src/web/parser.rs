56dff276950545ff07d59182dcde161e7fd4e19e
lemonsh <lemon@lemonsh.moe>
2022-07-24 00:03:52 +0200

    spacing

--- src/web/parser.rs
+++ src/web/parser.rs
@@ -54,7 +54,7 @@ pub fn textify(json: &str, webhook_name: &str) -> anyhow::Result<String> {
     if let Some(content) = wh.content {
         let content = content.trim().truncate_ellipse(450);
         for line in content.lines() {
-            write!(&mut buf, " {}\r\n", line)?;
+            write!(&mut buf, "  {}\r\n", line)?;
         }
     }
     for embed in wh.embeds {
@@ -66,38 +66,38 @@ pub fn textify(json: &str, webhook_name: &str) -> anyhow::Result<String> {
         if let Some(description) = embed.description.filter(|v| !v.is_empty()) {
             let description = description.trim().truncate_ellipse(450);
             for line in description.lines() {
-                write!(&mut buf, "  {}\r\n", line)?;
+                write!(&mut buf, "   {}\r\n", line)?;
             }
         }
         if let Some(fields) = embed.fields {
             for field in fields {
-                write!(&mut buf, "  + {}\r\n", field.name)?;
+                write!(&mut buf, "   + {}\r\n", field.name)?;
                 let value = field.value.trim().truncate_ellipse(450);
                 for line in value.lines() {
-                    write!(&mut buf, "   {}\r\n", line)?;
+                    write!(&mut buf, "     {}\r\n", line)?;
                 }
             }
         }
         if let Some(url) = embed.url.filter(|v| !v.is_empty()) {
-            write!(&mut buf, "  url: {}\r\n", url)?;
+            write!(&mut buf, "   url: {}\r\n", url)?;
         }
         if let Some(image) = embed.image.filter(|v| !v.url.is_empty()) {
-            write!(&mut buf, "  img: {}\r\n", image.url)?;
+            write!(&mut buf, "   img: {}\r\n", image.url)?;
         }
         if let Some(thumbnail) = embed.thumbnail.filter(|v| !v.url.is_empty()) {
-            write!(&mut buf, "  thumb: {}\r\n", thumbnail.url)?;
+            write!(&mut buf, "   thumb: {}\r\n", thumbnail.url)?;
         }
         if let Some(video) = embed.video.filter(|v| !v.url.is_empty()) {
-            write!(&mut buf, "  vid: {}\r\n", video.url)?;
+            write!(&mut buf, "   vid: {}\r\n", video.url)?;
         }
         if let Some(author) = embed.author.filter(|v| !v.name.is_empty()) {
-            write!(&mut buf, "  by: {}\r\n", author.name)?;
+            write!(&mut buf, "   by: {}\r\n", author.name)?;
         }
         if let Some(footer) = embed.footer.filter(|v| !v.text.is_empty()) {
-            write!(&mut buf, "  - {}\r\n", footer.text)?;
+            write!(&mut buf, "   - {}\r\n", footer.text)?;
         }
         if let Some(timestamp) = embed.timestamp.filter(|v| !v.is_empty()) {
-            write!(&mut buf, "  - {}\r\n", timestamp)?;
+            write!(&mut buf, "   - {}\r\n", timestamp)?;
         }
     }
 
